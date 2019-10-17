2445206cd306285600dfc22fbdbbe31ebd19fcda
Raewyn <raewynd@gmail.com>
2019-10-17 13:21:17 -0700

    wip fixing definitions

--- Apps/FlightSoftware/fprime/mk/configs/compiler/defines_HAL.mk
+++ Apps/FlightSoftware/fprime/mk/configs/compiler/defines_HAL.mk
@@ -1,4 +1,5 @@
 #define installation location of Halcogen package for TI R4
-PROJ_ROOT_LOC = ${HOME}/CubeRoverPackage/Apps/FlightSoftware/PrimaryFlightController/FlightMCU
+PROJ_ROOT_LOC = ${HOME}/CubeRover/CubeRoverPackage/Apps/FlightSoftware/PrimaryFlightController/FlightMCU
 
 HALCOGEN_ROOT_LOC = $(PROJ_ROOT_LOC)/HAL
+HALCOGEN_INCLUDES = $(HALCOGEN_ROOT_LOC)/include

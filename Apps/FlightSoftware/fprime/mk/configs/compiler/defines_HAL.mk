e7746c6d7e234831cea1423691dfb5b0a57acff1
cedriccorpa <cedric.corpadelafuente@astrobotic.com>
2019-10-17 15:33:53 -0400

    fix make

--- Apps/FlightSoftware/fprime/mk/configs/compiler/defines_HAL.mk
+++ Apps/FlightSoftware/fprime/mk/configs/compiler/defines_HAL.mk
@@ -1,4 +1,4 @@
 #define installation location of Halcogen package for TI R4
-PROJ_ROOT_LOC = $(HOME)/workspace_v9/PrimaryFlightMCU 
+PROJ_ROOT_LOC = ${HOME}/CubeRoverPackage/Apps/FlightSoftware/PrimaryFlightController/FlightMCU
 
 HALCOGEN_ROOT_LOC = $(PROJ_ROOT_LOC)/HAL

82fbef5a9e205653222d79a1b22c712e0e34a0e6
cedriccorpa <cedric.corpadelafuente@astrobotic.com>
2019-11-04 12:23:33 -0500

    wip

--- Apps/FlightSoftware/fprime/CubeRover/MotorControl/MotorControlComponentImpl.hpp
+++ Apps/FlightSoftware/fprime/CubeRover/MotorControl/MotorControlComponentImpl.hpp
@@ -14,41 +14,43 @@
 #define MotorControl_HPP
 
 #include "CubeRover/MotorControl/MotorControlComponentAc.hpp"
+#include "i2c.h"
 
 
 namespace CubeRover {
 
   #define MC_BUFFER_MAX_SIZE 16 // Maximum size of I2C buffer
+  #define PI 3.14159265
 
   typedef uint8_t MotorControlI2cRegId_t;
 
   typedef enum {
     I2C_ADDRESS = 0x01,
-    RELATIVE_TARGET_POSITION,
-    DIRECTION,
-    TARGET_SPEED,
-    CURRENT_POSITION,
-    MOTOR_CURRENT,
-    P_CURRENT,
-    I_CURRENT,
-    P_VELOCITY,
-    I_VELOCITY,
-    D_VELOCITY,
-    P_POSITION,
-    I_POSITION,
-    D_POSITION,
-    ACCELERATION,
-    DECELERATION,
-    EXECUTE_CMD,
-    CURRENT_VELOCITY,
-    ENABLE_DRIVER,
-    DISABLE_DRIVER,
-    RESET_CONTROLLER,
-    FAULT_REGISTER,
-    CLEAR_FAULT,
-    STATUS_REGISTER,
-    POSITION_SENSOR_CURRENT_COMBINATION,
-    MC_MAX_NUM_OF_ELEMENTS
+    I2C_RELATIVE_TARGET_POSITION,
+    I2C_DIRECTION,
+    I2C_TARGET_SPEED,
+    I2C_CURRENT_POSITION,
+    I2C_MOTOR_CURRENT,
+    I2C_P_CURRENT,
+    I2C_I_CURRENT,
+    I2C_P_VELOCITY,
+    I2C_I_VELOCITY,
+    I2C_D_VELOCITY,
+    I2C_P_POSITION,
+    I2C_I_POSITION,
+    I2C_D_POSITION,
+    I2C_ACCELERATION,
+    I2C_DECELERATION,
+    I2C_EXECUTE_CMD,
+    I2C_CURRENT_VELOCITY,
+    I2C_ENABLE_DRIVER,
+    I2C_DISABLE_DRIVER,
+    I2C_RESET_CONTROLLER,
+    I2C_FAULT_REGISTER,
+    I2C_CLEAR_FAULT,
+    I2C_STATUS_REGISTER,
+    I2C_POSITION_SENSOR_CURRENT_COMBINATION,
+    I2C_MC_MAX_NUM_OF_ELEMENTS
   }MotorControlI2cRegId;
 
   typedef enum {
@@ -178,12 +180,21 @@ namespace CubeRover {
     private:
       uint8_t txData[MC_BUFFER_MAX_SIZE];
       uint8_t rxData[MC_BUFFER_MAX_SIZE];
-      void writeMotorControlRegister(i2cBASE_t *i2c, const MotorControlI2cRegId id, uint32_t data);
-      MCError i2cMasterTransmit(i2cBASE_t *i2c, const uint32_t sadd, const uint32_t length, uint8_t * data);
-      MCError i2cMasterReceive(i2cBASE_t *i2c, const uint32_t sadd, const uint32_t length, uint8_t * data);
+      MCError writeMotorControlRegister(i2cBASE_t *i2c, const MotorControlI2cRegId id, const I2cSlaveAddress address, uint32_t data);
+      MCError i2cMasterTransmit(i2cBASE_t *i2c, const I2cSlaveAddress sadd, const uint32_t length, uint8_t * data);
+      MCError i2cMasterReceive(i2cBASE_t *i2c, const I2cSlaveAddress sadd, const uint32_t length, uint8_t * data);
       uint32_t getSizeData(const MotorControlI2cRegId id);
 
+      size_t headerSize();
+      size_t checksumSize();
+      void generateChecksumTable(const ChecksumType polynomial);
+      MCError computeChecksum8(uint8_t *data, const size_t bufferLength, MotorControlChecksum *checksum);
+      MCError packTransmitBuffer(const MotorControlI2cRegId id, const uint32_t data, const size_t dataLength);
       uint8_t checksumLookUpTable[256];
+
+      Motor_tick cmToMotorTicks(const Distance_cm dist);
+      MCError enableDrivers();
+      MCError disableDrivers();
 
       // left and right turn parameters
       uint8_t m_rightSpeed;

f95c566ec7df900e9ba2997cdf55985497b5c7a1
cedriccorpa <cedric.corpadelafuente@astrobotic.com>
2019-12-06 18:34:00 -0500

    clean up code

--- Apps/FlightSoftware/fprime/CubeRover/Top/Components.hpp
+++ Apps/FlightSoftware/fprime/CubeRover/Top/Components.hpp
@@ -20,10 +20,6 @@
 #include <Svc/ActiveLogger/ActiveLoggerImpl.hpp>
 #include <Svc/CmdDispatcher/CommandDispatcherImpl.hpp>
 
-
-// Include component exclusive to CubeRover project
-#include "MotorControl/MotorControlComponentImpl.hpp"
-
 // Block driver sequencing F-Prime and other sync events
 extern Drv::BlockDriverImpl blockDriver;
 

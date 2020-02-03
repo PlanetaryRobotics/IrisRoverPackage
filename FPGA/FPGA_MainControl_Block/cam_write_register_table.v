eb06c63a3cf3fb285496c4da8db93053fa38d661
Satat Ojha <satat.ojha@astrobotic.com>
2020-02-03 14:51:12 -0500

    cam write table

--- FPGA/FPGA_MainControl_Block/cam_write_register_table.v
+++ FPGA/FPGA_MainControl_Block/cam_write_register_table.v
@@ -1,15 +1,12 @@
-
-/*
-		name(of cmd to cam/cam-int)| addr(s)       | args for cam                    | args for interface                                     | instructions to camera
-		_______________________________________________________________________________________________________________________________________________________________
-
-		status register            | 0x00          | none                            | proc image cam 1/2, erase img proc, error bit, booting |  none
-		control reg						| 0x01			 | none									  | camera id, index                                       |
-		config cam1 & 2            | 0x02 and 0x03 | exposure val, shutter speed     | RGB, compression qf                                    |
-		error register             | 0x04          | none                            | read reg, clear reg                                    |
-		crop reg cam 1 & 2         | 0x05 and 0x06 | upper x, lowerx, upper, lower y | none                                                   |
-
+/* compression amount -> cam interface
+	 RGB or Grayscale? -> cam interface
+	 Exposure Value -> set of instr for img sensor
+	 Shutter Speed  -> set of instr for img sensor
+	 Cropping Amount  -> set of instr for img sensor
+	 Row bin  -> set of instr for img sensor
+	 Column bin  -> set of instr for img sensor
 */
+
 module cam_val_converter(input[2:0] val_type,
 								 input[2:0] step,
 								 input[31:0] val_in,
@@ -22,17 +19,79 @@ endmodule
 
 
 module cam_i2c_command_map(
-						          input[7:0]   reg_addr,
-						          input[127:0] reg_data,
+						       input[7:0]   reg_addr,
+						       input[63:0]  reg_data,
 									 input        valid_input,
-									 input		  instr_counter,
+									 input[4:0]   byte_counter,
 
-									 output [7:0]  cam_i2c_addr,
-									 output [15:0] cam_i2c_data,
+									 output [7:0] cam_i2c_byte,
 									 output        cam_i2c_output_valid,
-									 output        all_instr_out
-
+									 output        all_bytes_out
 						 );
+
+				reg[7:0] byte_out_r;
+
+				assign cam_i2c_byte = byte_out_r;
+
+				always@(*) begin
+						if((reg_addr == 8'h02) | (reg_addr == 8'h03))begin //shutter width upper, shutter width lower, and shutter delay
+								case(byte_counter) begin
+									5'b00000: byte_out_r <= 8'h08; //shutter width upper reg addr
+									5'b00001: byte_out_r <= 8'h00;
+									5'b00010: byte_out_r <= {4'b0000, reg_data[22:19]};
+
+									5'b00011: byte_out_r <= 8'h09; //shutter width lower reg addr
+									5'b00100: byte_out_r <= reg_data[18:11];
+									5'b00101: byte_out_r <= reg_data[10:3];
+
+									5'b00110: byte_out_r <= 8'h0C; //shutter delay addr
+									5'b00111: byte_out_r <= {3'b000, reg_data[35:31]};
+									5'b01000: byte_out_r <= reg_data[30:23];
+
+									5'b01001: byte_out_r <= 8'h22; // Row Address Mode (for Row Bin)
+									5'b01010: byte_out_r <= ;
+									5'b01011: byte_out_r <= ;
+
+									5'b01100: byte_out_r <= 8'h23; //Column Address Mode (for Col Bin)
+									5'b01101: byte_out_r <= ;
+									5'b01110: byte_out_r <= ;
+
+									5'b01111: byte_out_r <= 8'h05; //Horizontal Blanking
+									5'b1001: byte_out_r <= ;
+									5'b1001: byte_out_r <= ;
+
+									5'b1001: byte_out_r <= 8'h06; //Vertical Blanking
+									5'b1001: byte_out_r <= ;
+									5'b1001: byte_out_r <= ;
+
+
+									default: byte_out_r <= ;
+								endcase
+						end
+
+						else if((reg_addr == 8'h05) | (reg_addr == 8'h06)) begin
+						case(byte_counter) begin
+							4'b0000: byte_out_r <= 8'h01; //Row Start
+							4'b0001: byte_out_r <= ;
+							4'b0010: byte_out_r <= ;
+							4'b0011: byte_out_r <= 8'h02; //Column Start
+							4'b0100: byte_out_r <= ;
+							4'b0101: byte_out_r <= ;
+							4'b0110: byte_out_r <= 8'h03; // Row size
+							4'b0111: byte_out_r <= ;
+							4'b1000: byte_out_r <= ;
+							4'b1001: byte_out_r <= 8'h04; //column size
+							4'b1010: byte_out_r <= ;
+							4'b1011: byte_out_r <= ;
+							default: byte_out_r <= ;
+						endcase
+						end
+						else begin
+
+						end
+				end
+
+
 
 endmodule
 
@@ -40,25 +99,69 @@ endmodule
 module cam_write_register_table(input           sysClk, //clock
 										  /*from instruction buffer */
 										  input [7:0]     reg_addr,
-										  input [127:0]   reg_data,
+										  input [63:0]   reg_data,
 										  input    		   intr_valid_input,
 
-										  /*from slave copy buffer */
-										  input [15:0]   return_slave_copy,
-										  input           slave_copy_valid,
+											/*from cam interface */
+											input ready_for_next_byte;
 
 
 										  /* cam i2c stuff */
-										  output [7:0]  cam_i2c_addr,
-										  output [15:0] cam_i2c_data,
+										  output [7:0]  cam_i2c_byte_out,
+
 										  /* cam interface config stuff */
 										  output [1:0]  compression,
 										  output        RGB,
 
 										  /*all cam stuff*/
 										  output        cam_id,
+											output 				trigger,
+											output[15:0] 	trigger_index
 										  output        output_valid
 										  );
+
+					reg output_valid_r;
+					reg cam_id_r;
+					reg [1:0] compression_r;
+					reg RGB_r;
+					reg [7:0] cam_i2c_byte_out_r;
+
+
+					assign output_valid = output_valid_r;
+					assign cam_id = cam_id_r;
+					assign compression = compression_r;
+					assign RGB = RGB_r;
+					assign cam_i2c_byte_out = cam_i2c_byte_out_r;
+
+					wire [4:0] byte_counter_w;
+					reg  [4:0] byte_counter_r;
+
+					assign byte_counter_w = byte_counter_r;
+
+					/* i/o for the command map */
+					wire 		valid_input_for_commandmap;
+					wire[7:0] byte_out_from_commandmap;
+					wire byte_valid_out_from_commandmap;
+					wire all_instr_out_flag;
+
+
+					always@(posedge clock) begin
+							if(intr_valid_input) begin
+								cam_id_r <= (reg_addr == 8'h02);
+								if() begin
+
+								end
+								if(~reg_addr[2]) begin //for the configs
+										compression_r <= reg_data[1:0];
+										RGB_r <= reg_data[2];
+								end
+								else begin //for the crops
+
+								end
+							end
+					end
+
+
 
 
 
--- FPGA/FPGA_MainControl_Block/instr_data_buffer.v
+++ FPGA/FPGA_MainControl_Block/instr_data_buffer.v
@@ -1,49 +1,49 @@
 module instr_data_buffer(input sysClk,
 								 input [7:0] byte, // byte from spi interface
 								 input rx_valid,  // is the byte valid?
-								 
+
 								 output reg [7:0]   instruction,
-								 output reg [127:0] data,
+								 output reg [63:0] data,
 								 output reg         valid_buffer_for_camwrite,
 								 output reg         valid_buffer_for_read,
 								 output reg         valid_buffer_for_mem
 								 );
-		
-		
+
+
 		wire camwrite_cmd;
 		wire read_cmd;
 		wire mem_cmd;
-		
+
 		wire output_complete;
-		
-		reg [4:0] byte_counter;
-		
-		reg [135:0] next_byte_buffer;
-		wire[135:0] byte_buffer;
-		
+
+		reg [3:0] byte_counter;
+
+		reg [71:0] next_byte_buffer;
+		wire[71:0] byte_buffer;
+
 		assign byte_buffer = next_byte_buffer;
-		
+
 		assign output_complete = valid_buffer_for_camwrite | valid_buffer_for_read | valid_buffer_for_mem;
-		
-		
-		assign camwrite_cmd = (byte_buffer[135:128] == 2'h01) | (byte_buffer[135:128] == 2'h02) | (byte_buffer[135:128] == 2'h03) | (byte_buffer[135:128] == 2'h05) | (byte_buffer[135:128] == 2'h06);
-		assign read_cmd = (byte_buffer[135:128] == 2'h00) | (byte_buffer[135:128] == 2'h04);
-		assign mem_cmd = (byte_buffer[135:128] == 2'h07) | (byte_buffer[135:128] == 2'h08) | (byte_buffer[135:128] == 2'h09) | (byte_buffer[135:128] == 2'0A);
-		
-		
-		
-		
+
+
+		assign camwrite_cmd = (byte_buffer[71:64] == 2'h01) | (byte_buffer[71:64] == 2'h02) | (byte_buffer[71:64] == 2'h03) | (byte_buffer[71:64] == 2'h05) | (byte_buffer[71:64] == 2'h06) | (byte_buffer[71:64] == 2'h0B);
+		assign read_cmd = (byte_buffer[71:64] == 2'h00) | (byte_buffer[71:64] == 2'h04);
+		assign mem_cmd = (byte_buffer[71:64] == 2'h07) | (byte_buffer[71:64] == 2'h08) | (byte_buffer[71:64] == 2'h09) | (byte_buffer[71:64] == 2'0A);
+
+
+
+
 	   always @(posedge sysClk) begin
 			if(rx_valid & (~output_complete)) begin
 				next_byte_buffer <= ((byte_buffer | byte) << 8);
 				byte_counter <= byte_counter + 1;
 			end
 		end
-		
+
 		always @(posedge sysClk) begin
-			if(byte_counter == 5'10001) begin
-				instruction <= byte_buffer[135:128];
-				data <= byte_buffer[127:0];
+			if(byte_counter == 4'1001) begin
+				instruction <= byte_buffer[71:64];
+				data <= byte_buffer[63:0];
 				valid_buffer_for_camwrite <= camwrite_cmd;
 				valid_buffer_for_read <= read_cmd;
 				valid_buffer_for_mem <= mem_cmd;
@@ -57,4 +57,3 @@ module instr_data_buffer(input sysClk,
 			end
 		end
 endmodule
-								 

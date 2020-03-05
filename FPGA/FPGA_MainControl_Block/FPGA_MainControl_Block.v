6bbb03ea57eb8999df95aaedd50193f6e082a657
Satat Ojha <satat.ojha@astrobotic.com>
2020-03-05 15:13:25 -0500

    testing mem register

--- FPGA/FPGA_MainControl_Block/FPGA_MainControl_Block.v
+++ FPGA/FPGA_MainControl_Block/FPGA_MainControl_Block.v
@@ -40,16 +40,26 @@ module FPGA_MainControl_Block(input sysClk,
 //														output        valid_buffer_for_mem_MCB,
 
 														/*currently testing outputs from cam_write_register_table */
-														output[7:0] cam_i2c_byte_MCB,
-														output[1:0] compression_MCB,
-														output RGB_MCB,
-														output cam_id_MCB,
-														output[27:0] timestamp_MCB,
-														output       trigger_MCB,
-														output[15:0] trigger_index_MCB,
-														output hard_reset_MCB,
-														output cam_i2c_output_valid_MCB,
-														output cam_interface_output_valid_MCB
+														// output[7:0] cam_i2c_byte_MCB,
+														// output[1:0] compression_MCB,
+														// output RGB_MCB,
+														// output cam_id_MCB,
+														// output[27:0] timestamp_MCB,
+														// output       trigger_MCB,
+														// output[15:0] trigger_index_MCB,
+														// output hard_reset_MCB,
+														// output cam_i2c_output_valid_MCB,
+														// output cam_interface_output_valid_MCB
+
+														/*currently testing outputs from mem_register_table */
+														 output read_img_size_MCB,
+														 output [11:0] img_index_MCB,
+														 output stop_reading_img_flag_MCB,
+														 output reset_img_pointer_flag_MCB,
+														 output start_reading_img_flag_MCB,
+														 output start_erasing_img_flag_MCB,
+														 output [7:0]  byte_out_MCB,
+														 output byte_out_valid_MCB
 								  );
 								wire [7:0]  instruction;
 								wire [63:0] data;
@@ -63,7 +73,7 @@ module FPGA_MainControl_Block(input sysClk,
 													spi_byte,
 													spi_input_valid,
 													spi_byte_num,
-													
+
 													instruction,
 													data,
 													valid_buffer_for_camwrite,
@@ -71,26 +81,63 @@ module FPGA_MainControl_Block(input sysClk,
 													valid_buffer_for_mem
 										);
 
-										cam_write_register_table cwrt_test(
-										  .sysClk(sysClk),
-										  .reg_addr(instruction),
-										  .reg_data(data),
-										  .intr_valid_input(valid_buffer_for_camwrite),
-                                .ready_for_next_byte(ready_for_next_byte_MCB),
-										  .cam_i2c_byte_out(cam_i2c_byte_MCB),
-										  .compression(compression_MCB),
-										  .RGB(RGB_MCB),
-										  .cam_id(cam_id_MCB),
-										  .timestamp(timestamp_MCB),
-										  .trigger(trigger_MCB),
-										  .trigger_index(trigger_index_MCB),
-										  .hard_reset(hard_reset_MCB),
-										  .cam_i2c_output_valid(cam_i2c_output_valid_MCB),
-										  .cam_interface_output_valid(cam_interface_output_valid_MCB)
+
+
+										// cam_write_register_table cwrt_test(
+										//   .sysClk(sysClk),
+										//   .reg_addr(instruction),
+										//   .reg_data(data),
+										//   .intr_valid_input(valid_buffer_for_camwrite),
+                    //   .ready_for_next_byte(ready_for_next_byte_MCB),
+										//   .cam_i2c_byte_out(cam_i2c_byte_MCB),
+										//   .compression(compression_MCB),
+										//   .RGB(RGB_MCB),
+										//   .cam_id(cam_id_MCB),
+										//   .timestamp(timestamp_MCB),
+										//   .trigger(trigger_MCB),
+										//   .trigger_index(trigger_index_MCB),
+										//   .hard_reset(hard_reset_MCB),
+										//   .cam_i2c_output_valid(cam_i2c_output_valid_MCB),
+										//   .cam_interface_output_valid(cam_interface_output_valid_MCB)
+										// 	);
+
+										wire begin_numimg_flush;
+										wire [31:0] out_numimg;
+										wire out_numimg_valid;
+
+										num_img_stored_reg nisr(
+												nav_img_added_flag_MCB,
+												science_img_added_flag_MCB,
+												begin_numimg_flush,
+
+												out_numimg,
+												out_numimg_valid
+											);
+
+
+										mem_register_table mrt(
+														sysClk,
+														instruction,
+														data,
+														valid_buffer_for_mem,
+														jpg_size_MCB,
+														jpg_size_valid_MCB,
+														done_reading_img_flag_MCB,
+														done_erasing_img_flag_MCB,
+													  out_numimg,
+														out_numimg_valid,
+
+														begin_numimg_flush,
+														read_img_size_MCB,
+														img_index_MCB,
+														stop_reading_img_flag_MCB,
+														reset_img_pointer_flag_MCB,
+														start_reading_img_flag_MCB,
+														start_erasing_img_flag_MCB,
+														byte_out_MCB,
+														byte_out_valid_MCB
 											);
 
 
 
-
-							
 endmodule
--- FPGA/FPGA_MainControl_Block/FPGA_MainControl_Block_tb.v
+++ FPGA/FPGA_MainControl_Block/FPGA_MainControl_Block_tb.v
@@ -21,32 +21,90 @@ module main_control_block_tb();
 
 
 	 wire[71:0] full_buff;
-	 assign full_buff = 	72'b000000111010000110001110111111100110111110100110001101100001101010101110;
+	 //assign full_buff = 	72'b000000111010000110001110111111100110111110100110001101100001101010101110;
 		//0000,0011 |1010,0001,1000,1110,1111,1110,0110,1111,1010,0110,0011,0110,0001,1010,1010,1110
 
+	 //assign full_buff = 	72'b000000010000000000000000000111110101100110010101100110010101010010101011;
+	 //0000,0001 | 1101,0101,0010,1010,1001,1001,1010,1001,1001,1010,1111,1000,0000,0000,0000,0000
+
+	 //assign full_buff = 	72'b000001100000000000000000001010101010100001110011000000000001100000001001;
+	 //0000, 0110 | 0000, 0000,0000,0000,0010,1010,1010,1000,0111,0011,0000,0000,0001,1000,0000,1001
+
+	 assign full_buff = 72'b000010000000000000000000000000000000000000000000000000000000000000000000;
+	 //0000,1000 | 0000,0000,0000,0000,0000,0000,0000,0000,0000,0000,0000,0000,0000,0000,0000,0000
+
+	 reg nav_img_added_flag_r; // from mem interface block
+	 reg science_img_added_flag_r; //from mem interface block
+	
 	 wire[3:0] byte_counter;
 	 reg[3:0] byte_counter_s;
 
 	 assign byte_counter = byte_counter_s;
 
+	 	wire [23:0] jpg_size; //from mem interface block
+		wire jpg_size_valid; //from mem interface block
+		wire done_reading_img_flag; //from mem interface block
+		wire done_erasing_img_flag; //from mem interface block
+		wire nav_img_added_flag; // from mem interface block
+		wire science_img_added_flag; //from mem interface block
+
+		wire read_img_size;
+		wire [11:0] img_index;
+		wire stop_reading_img_flag;
+		wire reset_img_pointer_flag;
+		wire start_reading_img_flag;
+		wire start_erasing_img_flag;
+		wire [7:0]  byte_out;
+		wire byte_out_valid;
+
+			
+		assign nav_img_added_flag = nav_img_added_flag_r;
+		assign science_img_added_flag = science_img_added_flag_r;
+		
 	 FPGA_MainControl_Block dut(
 					.sysClk(sysClk),
 					.spi_byte(byte_in),
 					.spi_input_valid(rx_valid),
 					.spi_byte_num(byte_num),
-					.cam_i2c_byte_MCB(cam_i2c_byte),
-			 	   .compression_MCB(compression),
-			 	   .RGB_MCB(RGB),
-			 	   .cam_id_MCB(cam_id),
-			 	   .timestamp_MCB(timestamp),
-			 	   .trigger_MCB(trigger),
-			 	   .trigger_index_MCB(trigger_index),
-			 	   .cam_i2c_output_valid_MCB(cam_i2c_output_valid),
-			 	   .cam_interface_output_valid_MCB(cam_interface_output_valid)
+
+
+					.jpg_size_MCB(jpg_size),
+					.jpg_size_valid_MCB(jpg_size_valid),
+					.done_reading_img_flag_MCB(done_reading_img_flag),
+					.done_erasing_img_flag_MCB(done_erasing_img_flag),
+					.nav_img_added_flag_MCB(nav_img_added_flag),
+					.science_img_added_flag_MCB(science_img_added_flag),
+
+					.read_img_size_MCB(read_img_size),
+					.img_index_MCB(img_index),
+					.stop_reading_img_flag_MCB(stop_reading_img_flag),
+					.reset_img_pointer_flag_MCB(reset_img_pointer_flag),
+					.start_reading_img_flag_MCB(start_reading_img_flag),
+					.start_erasing_img_flag_MCB(start_erasing_img_flag),
+					.byte_out_MCB(byte_out),
+					.byte_out_valid_MCB(byte_out_valid)
 					);
 
 
 	 initial begin
+	  nav_img_added_flag_r = 1;
+		science_img_added_flag_r = 0;
+		#10
+		nav_img_added_flag_r = 1;
+		science_img_added_flag_r = 0;
+		#10
+		nav_img_added_flag_r = 1;
+		science_img_added_flag_r = 0;
+		#10
+		nav_img_added_flag_r = 0;
+		science_img_added_flag_r = 1;
+		#10
+		nav_img_added_flag_r = 1;
+		science_img_added_flag_r = 0;
+		#10
+		nav_img_added_flag_r = 0;
+		science_img_added_flag_r = 0;
+
 		byte_num = 4'b0000;
 		byte_counter_s = 4'b0000;
 		byte_in = 0;
@@ -59,12 +117,8 @@ module main_control_block_tb();
 
 
 		always begin
-
 			#10
 			sysClk = ~sysClk;
-
-
-
 		end
 
 		always@(posedge sysClk) begin

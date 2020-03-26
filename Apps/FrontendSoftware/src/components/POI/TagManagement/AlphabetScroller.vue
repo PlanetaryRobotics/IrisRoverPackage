93094bda6541f93a91348db6aeabb65848457f19
Caitlin Coyiuto <coyiutoc@gmail.com>
2020-03-26 00:33:59 -0400

    Base code for alphabet scroller on tag management.

--- Apps/FrontendSoftware/src/components/POI/TagManagement/AlphabetScroller.vue
+++ Apps/FrontendSoftware/src/components/POI/TagManagement/AlphabetScroller.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="AlphabetScroller">
-      Scrolly
+      <div class="letter" v-for="(letter, index) in alphabetList" :key="index">
+          {{letter}}
+      </div>
     </div> 
 </template>
 
@@ -8,14 +10,19 @@
 
 export default {
   name: "AlphabetScroller",
-  components: {
-   
-  },
   data() {
     return {
     }
   },
   computed: {
+    alphabetList() {
+      let set = new Set();
+      Array.from(this.$store.getters.POIByTagList.keys())
+            .forEach(elem => set.add(elem.getName()[0].toUpperCase()));
+      
+      return "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z".split(" ");
+      //return [...set];
+    }
   },
 }
 
@@ -27,8 +34,18 @@ export default {
 @import '@/styles/_typography.scss';
 
 .AlphabetScroller {
+  padding-top: 16px;
   background-color: $color-near-black;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.letter {
+  font-weight: 600;
+  font-size: (70vh/26);
+  color: $color-grey-dark;
 }
 
 </style>

67d4181c77ae8284081a868179133e950826d3e6
Justin Nguyen <justinnuwin@gmail.com>
2020-09-28 17:48:22 -0700

    Forgot to commit updated main.cpp to get everything running

--- Apps/FlightSoftware/PrimaryFlightController/FlightMCU/App/Main.cpp
+++ Apps/FlightSoftware/PrimaryFlightController/FlightMCU/App/Main.cpp
@@ -23,6 +23,18 @@ void vApplicationIdleHook( void ){
     for(uint32_t i=0; i<2000000; i++) asm("  NOP");
 }
 
+Drv::BlockDriverImpl blockDriver("blockDriver");
+int dividers[3] = {220000000, 2200000, 220000};
+Svc::RateGroupDriverImpl rateGroupDriver("rateGroupDriver", dividers, 3);
+// FIXME: The following needs the "context" parameters filled out
+Svc::ActiveRateGroupImpl rateGroupLowFreq("rateGroupLowFreq", {}, 0);   // 1Hz
+Svc::ActiveRateGroupImpl rateGroupMedFreq("rateGroupMedFreq", {}, 0);   // 100Hz
+Svc::ActiveRateGroupImpl rateGroupHiFreq("rateGroupHiFreq", {}, 0);     // 1kHz
+// FIXME: This module needs to be autocoded
+// extern Svc::TimeImpl cubeRoverTime("cubeRoverTime");
+Svc::TlmChanImpl tlmChan("tlmChan");
+Svc::CommandDispatcherImpl cmdDispatcher("cmdDispatcher");
+
 void main(void)
 {
 /* USER CODE BEGIN (3) */

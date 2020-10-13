0069d3056023554c5fcb4709bb0ff721894f5899
Justin Nguyen <justinnuwin@gmail.com>
2020-10-12 23:19:13 -0700

    Remove saving tmp file

--- Apps/FlightSoftware/fprime/install_libs.sh
+++ Apps/FlightSoftware/fprime/install_libs.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 mkdir -p ./Libs
-find . -path ./Libs -prune -o -print | grep -E "tiR4-ti-cgt-arm-bin/(.+)\.a$" | tee tmp.txt | xargs cp -vft ./Libs/
+find . -path ./Libs -prune -o -print | grep -E "tiR4-ti-cgt-arm-bin/(.+)\.a$" | xargs cp -vft ./Libs/

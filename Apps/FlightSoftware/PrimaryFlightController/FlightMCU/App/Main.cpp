05126cefcfe0f66bb470a409b31e36224be361ad
Justin Nguyen <justinnuwin@gmail.com>
2020-11-18 00:50:38 -0800

    Remove *.cpp files that are unused since CCS keeps trying to compile them >:(

--- Apps/FlightSoftware/PrimaryFlightController/FlightMCU/App/Main.cpp
+++ Apps/FlightSoftware/PrimaryFlightController/FlightMCU/App/Main.cpp
@@ -9,7 +9,6 @@
 #include "FreeRTOS.h"
 #include "os_task.h"
 #include "CubeRover/Top/Topology.hpp"
-#include "CubeRover/NetworkManager/CubeRoverNetworkManager.hpp"
 
 #include "gio.h"
 #include "i2c.h"
@@ -24,16 +23,9 @@ void vApplicationIdleHook(void) {
     //for(uint32_t i=0; i<2000000; i++) asm("  NOP");
 }
 
-using namespace Wf121;
-
-uint8_t g_testBuffer[RX_RING_BUFFER_SIZE];
 
 void main(void)
 {
-    uint16_t headerSize = 8;    // 8 bytes
-    uint16_t byteRead = 0;
-    uint32_t payloadSize = 0;
-
     /* USER CODE BEGIN (3) */
     gioInit();
 
@@ -41,26 +33,6 @@ void main(void)
     sciInit();
 
     constructApp();
-    /*CubeRoverNetworkManager wf121;
-    while(1){
-        wf121.UpdateNetworkManager();
-
-        wf121.ReceiveUdpData(g_testBuffer, headerSize, &byteRead, UdpReadMode::WAIT_UNTIL_READY | UdpReadMode::PEEK_READ, 10);
-
-        if(byteRead == headerSize){
-            // get size of the received packet
-            // check how big the packet actually is, then consume the bytes from the ring buffer
-            memcpy(&payloadSize, g_testBuffer+4, sizeof(payloadSize));
-            // FIXME: payloadsize is U32 but in UDP header length is only bytes 4 and 5 (U16)
-            wf121.ReceiveUdpData(g_testBuffer, payloadSize, &byteRead, UdpReadMode::WAIT_UNTIL_READY | UdpReadMode::NORMAL_READ, 10);
-
-            // send back signal quality
-            g_testBuffer[8] = wf121.GetSignalRssi();
-            g_testBuffer[9] = wf121.GetSignalNoiseRatio();
-            sciSend(scilinREG, payloadSize, g_testBuffer);
-            wf121.SendUdpData(g_testBuffer, payloadSize, 10000);
-        }
-    }*/
 
     vTaskStartScheduler();
 

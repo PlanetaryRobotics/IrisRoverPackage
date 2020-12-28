f904f16efc8675acb5b3327efc4f9a94bf09a304
Alec <alecjprobst@gmail.com>
2020-12-28 16:11:04 -0500

    Added RM46L fix from other branches, changed Os/mod.mk to include File.hpp in FreeRTOS folder, copied updated build_libs.sh from other branches

--- Apps/FlightSoftware/fprime/mk/configs/compiler/defines_common.mk
+++ Apps/FlightSoftware/fprime/mk/configs/compiler/defines_common.mk
@@ -1,3 +1,8 @@
 COMMON_DEFINES  := -DISF -DBUILD_$(BUILD)
+<<<<<<< HEAD
 CUBEROVER_PACKAGE_TOP_LEVEL=$(HOME)/justin-ccs9-workspace/CubeRoverPackage
 FPRIME_ROOT_LOC=$(HOME)/justin-ccs9-workspace/CubeRoverPackage/Apps/FlightSoftware/fprime
+=======
+CUBEROVER_PACKAGE_TOP_LEVEL=/home/alec/CubeRoverPackage
+FPRIME_ROOT_LOC=/home/alec/CubeRoverPackage/Apps/FlightSoftware/fprime
+>>>>>>> Added RM46L fix from other branches, changed Os/mod.mk to include File.hpp in FreeRTOS folder, copied updated build_libs.sh from other branches

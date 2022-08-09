ee4eca02c3661e2ef73a02687cd9a6f85a61f6f4
Connor Colombo <connor.caxd@gmail.com>
2022-08-09 19:12:48 -0400

    Changed default debug comms state.
    // [CWC - 08/09/22] We only observed FM1 working without bootloop present with debug messages being
    // printed, so we should preserve this as the default behavior since we'll have so little time with
    // the rover during RC3 upload to test and debug. Also, this is an arguably better default anyway
    // since it lets us: A.) capture on-boot messages like "reset because" that would be gone by the
    // time we could send the "enable debug messages" command. B.) if there's a comms issue that
    // prevents us from talking *to* the rover, this might help.

--- Apps/FlightSoftware/Watchdog/include/comms/debug_comms.h
+++ Apps/FlightSoftware/Watchdog/include/comms/debug_comms.h
@@ -5,6 +5,14 @@
 #include "common.h"
 #include "assert.h"
 
+// [CWC - 08/09/22] We only observed FM1 working without bootloop present with debug messages being
+// printed, so we should preserve this as the default behavior since we'll have so little time with
+// the rover during RC3 upload to test and debug. Also, this is an arguably better default anyway
+// since it lets us: A.) capture on-boot messages like "reset because" that would be gone by the
+// time we could send the "enable debug messages" command. B.) if there's a comms issue that
+// prevents us from talking *to* the rover, this might help. 
+#define ENABLE_DEBUG_ONLY_CODE
+
 #ifdef __cplusplus
 extern "C"
 {

ad5913812204b9eccb13e8a98115eafed3c21d30
Connor <connor.caxd@gmail.com>
2022-08-13 14:23:52 -0400

    Radio: Tweaked Iris MAC address.

--- Apps/FlightSoftware/Radio/compile.bat
+++ Apps/FlightSoftware/Radio/compile.bat
@@ -4,7 +4,7 @@ echo Compiling the WF121 project...
 C:\Bluegiga\wf121-1.4.1-91\bin\bgbuild.exe .\project.xml
 
 echo Correcting the MAC address...
-:: Change MAC address to Bluegiga (00:07:80) + Iris (12:15) + IF (ff)
-C:\Bluegiga\wf121-1.4.1-91\bin\changemac.exe .\build\iris_wifi_radio__auto_connect.hex 00:07:80:12:15:ff
+:: Change MAC address to Bluegiga (00:07:80) + FF (ff) + Iris (12:15)
+C:\Bluegiga\wf121-1.4.1-91\bin\changemac.exe .\build\iris_wifi_radio__auto_connect.hex 00:07:80:ff:12:15
 
 pause

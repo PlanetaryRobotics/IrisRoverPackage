9146127849e0e87abe784a49e57397adfe5151f0
Connor <connor.caxd@gmail.com>
2022-08-13 15:32:42 -0400

    Radio: Tweaked MAC address (changed middle FF to C5 (Carnegie Space)).

--- Apps/FlightSoftware/Radio/compile.bat
+++ Apps/FlightSoftware/Radio/compile.bat
@@ -4,7 +4,7 @@ echo Compiling the WF121 project...
 C:\Bluegiga\wf121-1.4.1-91\bin\bgbuild.exe .\project.xml
 
 echo Correcting the MAC address...
-:: Change MAC address to Bluegiga (00:07:80) + FF (ff) + Iris (12:15)
-C:\Bluegiga\wf121-1.4.1-91\bin\changemac.exe .\build\iris_wifi_radio__auto_connect.hex 00:07:80:ff:12:15
+:: Change MAC address to Bluegiga (00:07:80) + Carnegie Space (c5) + Iris (12:15)
+C:\Bluegiga\wf121-1.4.1-91\bin\changemac.exe .\build\iris_wifi_radio__auto_connect.hex 00:07:80:c5:12:15
 
 pause

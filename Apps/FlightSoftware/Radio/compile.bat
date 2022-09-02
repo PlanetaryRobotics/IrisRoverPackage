a4f462b8f1798212faee6e5a34a7a35efc6953b0
Connor <connor.caxd@gmail.com>
2022-09-02 16:54:09 -0400

    Radio: Started tracking variable_memory_usage with Radio FW builds since it is actually quite useful for determining what the order of effects would be from a stack overflow into DIM memory space (i.e. good for tracking with build files so if we thinks this has happened on the Moon, we can have better luck figuring out the actual effects).

--- Apps/FlightSoftware/Radio/compile.bat
+++ Apps/FlightSoftware/Radio/compile.bat
@@ -7,4 +7,7 @@ echo Correcting the MAC address...
 :: Change MAC address to Bluegiga (00:07:80) + Carnegie Space (c5) + Iris (12:15)
 C:\Bluegiga\wf121-1.4.1-91\bin\changemac.exe .\build\iris_wifi_radio__auto_connect.hex 00:07:80:c5:12:15
 
+echo Cleaning Up...
+move .\variable_memory_usage.txt .\build
+
 pause

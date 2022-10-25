7508fb543270fc5497668ad8194ca4f25fb69b0e
iris-hermes-mobile <iris.hermes.mobile@gmail.com>
2022-10-25 12:31:16 -0400

    Bump patch b/c memory allocation works now (new buffers now fit into memory.

--- Apps/FlightSoftware/PrimaryFlightController/FlightMCU/Include/Version.h
+++ Apps/FlightSoftware/PrimaryFlightController/FlightMCU/Include/Version.h
@@ -11,6 +11,6 @@
 
 static const unsigned VERSION_MAJOR = 5; // Version: ++ when drafting a new Release Candidate (for a new upload opportunity)
 static const unsigned VERSION_MINOR = 0; // Subversion: ++ when a new major feature has been added / made to work
-static const unsigned VERSION_REVISION = 1; // Patch: ++ when you make a change and want to reflect that.
+static const unsigned VERSION_REVISION = 2; // Patch: ++ when you make a change and want to reflect that.
 
 #endif // _VERSION_H_

c76aac665041309898f8fe3b1f0db4282cf088a4
zCoCo <connor.caxd@gmail.com>
2022-10-25 12:24:26 -0400

    FW/SVC: Tweaked svc component configs to free up some memory.

--- Apps/FlightSoftware/fprime/Svc/TlmChan/TlmChanImplCfg.hpp
+++ Apps/FlightSoftware/fprime/Svc/TlmChan/TlmChanImplCfg.hpp
@@ -15,7 +15,6 @@
 
 // Anonymous namespace for configuration parameters
 
-
 // The parameters below provide for tuning of the hash function used to
 // write and read entries in the database. The has function is very simple;
 // It first takes the telemetry ID and does a modulo computation with
@@ -39,38 +38,43 @@
 //        ... (Other buckets in the slot)
 //     The number of buckets assigned to each slot can be checked for balance.
 
-namespace {
+namespace
+{
 
-    enum {
-        TLMCHAN_NUM_TLM_HASH_SLOTS = 26, // !< Number of slots in the hash table.
-                                        // Works best when set to about twice the number of components producing telemetry
+    enum
+    {
+        TLMCHAN_NUM_TLM_HASH_SLOTS = 25, // !< Number of slots in the hash table.
+                                         // Works best when set to about twice the number of components producing telemetry
         // At the time of writing, there are only 12 components producing telem. So 2*12=24 + some room (it used to be 30)=26
-        TLMCHAN_HASH_MOD_VALUE = 250,    // !< The modulo value of the hashing function.
-                                        // Should be set to a little below the ID gaps to spread the entries around
+        TLMCHAN_HASH_MOD_VALUE = 250, // !< The modulo value of the hashing function.
+                                      // Should be set to a little below the ID gaps to spread the entries around
 
-        TLMCHAN_HASH_BUCKETS = 74       // !< Buckets assignable to a hash slot.
-                                        // Buckets must be >= number of telemetry channels in system
+        TLMCHAN_HASH_BUCKETS = 72 // !< Buckets assignable to a hash slot.
+                                  // Buckets must be >= number of telemetry channels in system
         // At the time of writing, there are 71 total telemetry channels in the system (used GSW to count across all FPrime XML):
         /*
         Telemetry Channels in BlockDriver: 1
         Telemetry Channels in RateGroupDriver: 0
-        Telemetry Channels in ActiveRateGroup-RateGroupLowFreq: 2
-        Telemetry Channels in ActiveRateGroup-RateGroupMedFreq: 2
-        Telemetry Channels in ActiveRateGroup-RateGroupHiFreq: 2
+        *Telemetry Channels in ActiveRateGroup-RateGroupLowFreq: 2
+        *Telemetry Channels in ActiveRateGroup-RateGroupMedFreq: 2
+        *Telemetry Channels in ActiveRateGroup-RateGroupHiFreq: 2
         Telemetry Channels in CubeRoverTime: 0
         Telemetry Channels in TlmChan: 0
         Telemetry Channels in CommandDispatcher: 2
-        Telemetry Channels in GroundInterface: 13
-        Telemetry Channels in NetworkManager: 6
+        *Telemetry Channels in GroundInterface: 13
+        *Telemetry Channels in NetworkManager: 6
         Telemetry Channels in ActiveLogger: 0
-        Telemetry Channels in Navigation: 5
-        Telemetry Channels in MotorControl: 12
-        Telemetry Channels in Imu: 6
-        Telemetry Channels in WatchDogInterface: 15
-        Telemetry Channels in Camera: 5
-        */ // ... So, 74 plays it a little safe (in case any are added last minute but we forget to update this).
-    };
+        *Telemetry Channels in Navigation: 5
+        *Telemetry Channels in MotorControl: 12
+        *Telemetry Channels in Imu: 6
+        *Telemetry Channels in WatchDogInterface: 15
+        *Telemetry Channels in Camera: 5
 
+        But only those with *s are connected to `TlmChan` in Top, so this
+        number could go as low as: 68 + some room = 70
+        */
+        // ... So, 72 plays it a little safe (in case any are added last minute but we forget to update this).
+    };
 
 }
 

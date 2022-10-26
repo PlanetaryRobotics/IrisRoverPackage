c3e4b1d9e4e34fbc53be8a178ec197823953b4f8
iris-hermes-mobile <iris.hermes.mobile@gmail.com>
2022-10-26 07:18:15 -0400

    RC5: Added initial collection of docs.

---
+++ Bins/RC5/FSW/Hercules/CubeRover/CubeRoverPorts/FileDownlink/subdir_vars.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CPP_SRCS += \
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/CubeRover/CubeRoverPorts/FileDownlink/FileDownlinkPortAc.cpp 
+
+OBJS += \
+./CubeRover/CubeRoverPorts/FileDownlink/FileDownlinkPortAc.obj 
+
+CPP_DEPS += \
+./CubeRover/CubeRoverPorts/FileDownlink/FileDownlinkPortAc.d 
+
+OBJS__QUOTED += \
+"CubeRover/CubeRoverPorts/FileDownlink/FileDownlinkPortAc.obj" 
+
+CPP_DEPS__QUOTED += \
+"CubeRover/CubeRoverPorts/FileDownlink/FileDownlinkPortAc.d" 
+
+CPP_SRCS__QUOTED += \
+"/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/CubeRover/CubeRoverPorts/FileDownlink/FileDownlinkPortAc.cpp" 
+
+
---
+++ Bins/RC5/FSW/Hercules/CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.d
@@ -0,0 +1,77 @@
+# FIXED
+
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.cpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Cfg/Config.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/PrimaryFlightController/FlightMCU/Include/CubeRoverFPrimeConfig.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/PrimaryFlightController/FlightMCU/Include/CubeRoverConfig.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Types/Assert.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Types/BasicTypes.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Types/TIR4/StandardTypes.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/stdint.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/__config
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/__config_site
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/__libcxx_extra.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdint.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/assert.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_ti_config.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/linkage.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Types/Serializable.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/cstring
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/string.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/string.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/cstdio
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/stdio.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdio.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdarg.h
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Port/InputPortBase.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Obj/ObjBase.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Comp/PassiveComponentBase.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Port/PortBase.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Port/OutputPortBase.hpp
+CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.obj: /home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Types/StringType.hpp
+
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.cpp: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Cfg/Config.hpp: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/PrimaryFlightController/FlightMCU/Include/CubeRoverFPrimeConfig.hpp: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/PrimaryFlightController/FlightMCU/Include/CubeRoverConfig.hpp: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Types/Assert.hpp: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Types/BasicTypes.hpp: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Types/TIR4/StandardTypes.hpp: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/stdint.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/__config: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/__config_site: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/__libcxx_extra.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdint.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_stdint40.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/stdint.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/cdefs.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_types.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_types.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/machine/_stdint.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/sys/_stdint.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/assert.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/_ti_config.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/linkage.h: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Types/Serializable.hpp: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/CubeRover/CubeRoverPorts/IMUData/IMUDataPortAc.hpp: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/cstring: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/string.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/string.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/cstdio: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/libcxx/stdio.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdio.h: 
+/home/iris/ti/ccs910/ccs/tools/compiler/ti-cgt-arm_18.12.2.LTS/include/stdarg.h: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Port/InputPortBase.hpp: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Obj/ObjBase.hpp: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Comp/PassiveComponentBase.hpp: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Port/PortBase.hpp: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Port/OutputPortBase.hpp: 
+/home/iris/Desktop/Working_folders/RC/Apps/FlightSoftware/fprime/Fw/Types/StringType.hpp: 

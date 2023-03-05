7848592a992ad2f14a9f0904ccca237b01acd1f9
zCoCo <connor.caxd@gmail.com>
2023-03-05 12:51:17 -0500

    GDS: Added dot access to inner modules of transceiver and codec.

--- Apps/GroundSoftware/IrisBackendv3/codec/__init__.py
+++ Apps/GroundSoftware/IrisBackendv3/codec/__init__.py
@@ -9,7 +9,7 @@ corresponding `DataStandards` and exporting a properly formatted stream of bytes
 to the `Transceiver` layer.
 
 @author: Connor W. Colombo (CMU)
-@last-updated: 04/08/2021
+@last-updated: 03/03/2023
 """
 
 # Expose internal modules (allow them to be accessed directly):
@@ -18,3 +18,11 @@ from .magic import *
 from .fsw_data_codec import *
 
 from .exceptions import *
+
+# Expose important codec modules as modules (so they can be dot-accessed when
+# including IrisBackendv3 as a package):
+from . import metadata
+from . import packet
+from . import payload_collection
+from . import payload
+from . import settings
--- Apps/GroundSoftware/IrisBackendv3/transceiver/__init__.py
+++ Apps/GroundSoftware/IrisBackendv3/transceiver/__init__.py
@@ -3,7 +3,7 @@
 This package handles all 
 
 @author: Connor W. Colombo (CMU)
-@last-updated: 05/13/2022
+@last-updated: 03/03/2023
 """
 
 # Expose internal modules (allow them to be accessed directly):
@@ -12,3 +12,9 @@ from .endec import *
 from .transceiver import *
 from .slip_transceiver import *
 from .pcap_transceiver import *
+
+# Keep specialized modules still wrapped in a namespace (make them dot
+# accessible from root):
+from . import exceptions
+from . import prebuilts
+from . import settings

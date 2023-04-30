c287e56bbd840aa7419278324b44ecc45d57144e
zCoCo <connor.caxd@gmail.com>
2023-04-30 01:40:36 -0400

    IPC: Bugfix to ipc_app_sync_pub to make the queue fill quickly.

--- Apps/GroundSoftware/scripts/testing/ipc_demo/ipc_sync_pub.py
+++ Apps/GroundSoftware/scripts/testing/ipc_demo/ipc_sync_pub.py
@@ -25,6 +25,7 @@ IB3.init_from_latest()
 xcvrLoggerLevel('NOTICE')
 xcvr = IB3.transceiver.prebuilts.build_xcvr_by_name(
     'PCAP-RC9.5.3-2MIN-IMU',
+    fixed_period_ms=10,  # keep it short so the queue fills up
     log_on_receive=False
 )
 xcvr.begin()

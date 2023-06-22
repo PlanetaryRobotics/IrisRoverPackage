2e888cfa27e0fda6b091377ec50b5c0a61a7a07d
zCoCo <connor.caxd@gmail.com>
2023-06-22 18:11:24 -0400

    GDS-GUI: CommandLine now docks at bottom correctly.

--- Apps/GroundSoftware/ipc_apps/gui_lib/layout.py
+++ Apps/GroundSoftware/ipc_apps/gui_lib/layout.py
@@ -122,7 +122,7 @@ class GuiAIO(html.Div):
                     'display': 'flex',
                     'flex-direction': 'column',
                     'justify-content': 'space-between',
-                    'height': '100%'
+                    'height': '100vh'
                 })
             ],
             style={

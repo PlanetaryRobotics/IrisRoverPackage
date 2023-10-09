2a6ef9c0cefbb39363a087edc586bccd89b6ea1b
iris-hermes-mobile <iris.hermes.mobile@gmail.com>
2023-10-09 17:23:52 -0400

    VER: Bumped Herc version to 11.1.0 to reflect HAL reconfig.

--- Apps/FlightSoftware/PrimaryFlightController/FlightMCU/Include/Version.h
+++ Apps/FlightSoftware/PrimaryFlightController/FlightMCU/Include/Version.h
@@ -9,8 +9,8 @@
  ******************************************************************************
  *****************************************************************************/
 
-static const unsigned VERSION_MAJOR = 9;    // Version: ++ when drafting a new Release Candidate (for a new upload opportunity)
-static const unsigned VERSION_MINOR = 5;    // Subversion: ++ when a new major feature has been added / made to work
-static const unsigned VERSION_REVISION = 4; // Patch: ++ when you make a change and want to reflect that.
+static const unsigned VERSION_MAJOR = 11;    // Version: ++ when drafting a new Release Candidate (for a new upload opportunity)
+static const unsigned VERSION_MINOR = 1;    // Subversion: ++ when a new major feature has been added / made to work
+static const unsigned VERSION_REVISION = 0; // Patch: ++ when you make a change and want to reflect that.
 
 #endif // _VERSION_H_

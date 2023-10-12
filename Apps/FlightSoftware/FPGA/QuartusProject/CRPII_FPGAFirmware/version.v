1a633a7edf9db96a00a3143c3c946564c91de3b8
zCoCo <connor.caxd@gmail.com>
2023-10-11 21:40:48 -0400

    FPGA: Switched CameraSensorInterface Transaction FSM to new simplified synchronous architecture.

--- Apps/FlightSoftware/FPGA/QuartusProject/CRPII_FPGAFirmware/version.v
+++ Apps/FlightSoftware/FPGA/QuartusProject/CRPII_FPGAFirmware/version.v
@@ -5,7 +5,7 @@ module IrisFpgaFwVersion
     // Change the version for the Iris Camera FPGA FW information here:
     parameter integer FW_VER_MAJ = 8'd11,    // Version: ++ when drafting a new Release Candidate (for a new upload opportunity)
     parameter integer FW_VER_MIN = 8'd1,     // Subversion: ++ when a new major feature has been added / made to work
-    parameter integer FW_VER_PATCH = 8'd0    // Patch: ++ when you make a change and want to reflect that.
+    parameter integer FW_VER_PATCH = 8'd1    // Patch: ++ when you make a change and want to reflect that.
 )   
 (
     output wire [7:0] major,

8eacea173ed66f82c05b6fae3b743310c4eb3efd
zCoCo <connor.caxd@gmail.com>
2023-10-17 19:27:34 -0400

    INFRA: IMU linted.

--- Apps/FlightSoftware/fprime/CubeRover/IMU/IMUComponent.hpp
+++ Apps/FlightSoftware/fprime/CubeRover/IMU/IMUComponent.hpp
@@ -23,86 +23,82 @@
 #include "l3gd20h.h"
 
 // TODO: Make these configureable in flight
-#define IMU_LPF_COEFF                 0.01
-#define MAX_ROVER_PITCH_ANGLE         40
-#define MAX_ROVER_ROLL_ANGLE          40
+#define IMU_LPF_COEFF 0.01
+#define MAX_ROVER_PITCH_ANGLE 40
+#define MAX_ROVER_ROLL_ANGLE 40
 
-namespace CubeRover {
+namespace CubeRover
+{
 
-    class IMUComponentImpl :
-      public IMUComponentBase
-    {
-    public:
+  class IMUComponentImpl : public IMUComponentBase
+  {
+  public:
+    // ----------------------------------------------------------------------
+    // Construction, initialization, and destruction
+    // ----------------------------------------------------------------------
 
-      // ----------------------------------------------------------------------
-      // Construction, initialization, and destruction
-      // ----------------------------------------------------------------------
-
-      //! Construct object Imu
-      //!
-      IMUComponentImpl(
+    //! Construct object Imu
+    //!
+    IMUComponentImpl(
 #if FW_OBJECT_NAMES == 1
-          const char *const compName /*!< The component name*/
+        const char *const compName /*!< The component name*/
 #else
-          void
+        void
 #endif
-      );
+    );
 
-      //! Initialize object Imu
-      //!
-      void init(
-          const NATIVE_INT_TYPE instance = 0 /*!< The instance number*/
-      );
+    //! Initialize object Imu
+    //!
+    void init(
+        const NATIVE_INT_TYPE instance = 0 /*!< The instance number*/
+    );
 
-      //! Destroy object Imu
-      //!
-      ~IMUComponentImpl(void);
+    //! Destroy object Imu
+    //!
+    ~IMUComponentImpl(void);
 
-      
-      // ----------------------------------------------------------------------
-      // Handler implementations for user-defined typed input ports
-      // ----------------------------------------------------------------------
+    // ----------------------------------------------------------------------
+    // Handler implementations for user-defined typed input ports
+    // ----------------------------------------------------------------------
 
-      //! Handler implementation for schedIn
-      //!
-      void schedIn_handler(const NATIVE_INT_TYPE portNum, /*!< The port number*/
-                           NATIVE_UINT_TYPE context /*!< The call order*/);
+    //! Handler implementation for schedIn
+    //!
+    void schedIn_handler(const NATIVE_INT_TYPE portNum, /*!< The port number*/
+                         NATIVE_UINT_TYPE context /*!< The call order*/);
 
-      //! Handler implementation for PingIn
-      //!
-      void PingIn_handler(const NATIVE_INT_TYPE portNum, /*!< The port number*/
-                          U32 key /*!< Value to return to pinger*/);
+    //! Handler implementation for PingIn
+    //!
+    void PingIn_handler(const NATIVE_INT_TYPE portNum, /*!< The port number*/
+                        U32 key /*!< Value to return to pinger*/);
 
+    // ----------------------------------------------------------------------
+    // Command handler implementations
+    // ----------------------------------------------------------------------
 
-      // ----------------------------------------------------------------------
-      // Command handler implementations
-      // ----------------------------------------------------------------------
+    //! Implementation for Imu_ReportData command handler
+    //! A command to force an IMU data.
+    void IMU_ReportData_cmdHandler(const FwOpcodeType opCode, /*!< The opcode*/
+                                   const U32 cmdSeq /*!< The command sequence number*/);
 
-      //! Implementation for Imu_ReportData command handler
-      //! A command to force an IMU data.
-      void IMU_ReportData_cmdHandler(const FwOpcodeType opCode, /*!< The opcode*/
-                                     const U32 cmdSeq /*!< The command sequence number*/);
+    //! Implementation for IMU_TurnOn command handler
+    //! A command to turn on the hardware
+    void IMU_TurnOn_cmdHandler(const FwOpcodeType opCode, /*!< The opcode*/
+                               const U32 cmdSeq /*!< The command sequence number*/);
 
-      //! Implementation for IMU_TurnOn command handler
-      //! A command to turn on the hardware
-      void IMU_TurnOn_cmdHandler(const FwOpcodeType opCode, /*!< The opcode*/
-                                 const U32 cmdSeq /*!< The command sequence number*/);
+    //! Implementation for IMU_TurnOff command handler
+    //! A command to turn off the hardware
+    void IMU_TurnOff_cmdHandler(const FwOpcodeType opCode, /*!< The opcode*/
+                                const U32 cmdSeq /*!< The command sequence number*/);
 
-      //! Implementation for IMU_TurnOff command handler
-      //! A command to turn off the hardware
-      void IMU_TurnOff_cmdHandler(const FwOpcodeType opCode, /*!< The opcode*/
-                                  const U32 cmdSeq /*!< The command sequence number*/);
+  private:
+    void computePitchRoll(float32 *pitch, float32 *roll);
 
-    private:
-
-      void computePitchRoll(float32 *pitch, float32 *roll);
-      
-      struct AccVector m_acc;
-      struct GyroVector m_gyro;
-      float32 m_lpfAccX;
-      float32 m_lpfAccY;
-      float32 m_lpfAccZ;
-    };
+    struct AccVector m_acc;
+    struct GyroVector m_gyro;
+    float32 m_lpfAccX;
+    float32 m_lpfAccY;
+    float32 m_lpfAccZ;
+  };
 
 } // end namespace CubeRover
 

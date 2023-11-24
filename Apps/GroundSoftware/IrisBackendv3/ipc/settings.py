a59d23538955ccb058c77f0ff806c15cef8bc115
zCoCo <connor.caxd@gmail.com>
2023-11-24 18:08:46 -0500

    GDS: Switched IBv3 key settings to ENV for docker.

--- Apps/GroundSoftware/IrisBackendv3/ipc/settings.py
+++ Apps/GroundSoftware/IrisBackendv3/ipc/settings.py
@@ -7,16 +7,24 @@ Configurable settings for the IPC layer.
 @author: Connor W. Colombo (CMU)
 @last-updated: 02/27/2022
 """
+import os
+from typing import Final
+_KEY_ADDR_BASE: Final[str] = 'IBv3-ipc--'
 
+# Base defaults:
 settings = {
     'IP': '127.0.0.1',  # localhost
     # Key used to sign all pickled IPC messages.
     # All process running locally should be given the same session key.
-    # (!!TODO: make this settable from console for all IPC apps).
     'SESSION_KEY': 'test-session-key',
     # App Name used in Logs:
     'app_name': 'IpcApp'
 }
+
+# Override settings with OS Environment variables if applicable:
+for key in settings.keys():
+    if _KEY_ADDR_BASE+key in os.environ:
+        settings[key] = os.getenv(_KEY_ADDR_BASE+key)
 
 
 def set_ip(ip: str):
@@ -24,5 +32,3 @@ def set_ip(ip: str):
     Sets the IP for all IPC.
     """
     settings['IP'] = ip
-
-# *TODO: Make all these^ settable via cli args (opts?)
--- Apps/GroundSoftware/IrisBackendv3/transceiver/settings.py
+++ Apps/GroundSoftware/IrisBackendv3/transceiver/settings.py
@@ -7,11 +7,20 @@ Configurable settings for the IPC layer.
 @author: Connor W. Colombo (CMU)
 @last-updated: 07/03/2022
 """
+import os
+from typing import Final
+_KEY_ADDR_BASE: Final[str] = 'IBv3-xcvr--'
 
+# Base defaults:
 settings = {
     'yamcs_username': 'iris',
     'yamcs_password': 'notapassword'  # set the creds during each session.
 }
+
+# Override settings with OS Environment variables if applicable:
+for key in settings.keys():
+    if _KEY_ADDR_BASE+key in os.environ:
+        settings[key] = os.getenv(_KEY_ADDR_BASE+key)
 
 
 def set_yamcs_credentials(user, password):
@@ -20,5 +29,3 @@ def set_yamcs_credentials(user, password):
     """
     settings['yamcs_username'] = user
     settings['yamcs_password'] = password
-
-# *TODO: Make all these^ settable via cli args (opts?)
